e1ded29689abd974b064792abf86a188cb8d9162
Hiroki Osame <hiroki.osame@gmail.com>
2021-12-17 02:22:13 -0500

    docs: tsdocs for schema properties

--- src/types.ts
+++ src/types.ts
@@ -3,11 +3,54 @@ export type TypeFunction<ReturnType = any> = (value: any) => ReturnType;
 type TypeFunctionArray<ReturnType = any> = readonly [TypeFunction<ReturnType>];
 
 type FlagSchemaBase<TF> = {
+	/**
+	Type of the flag as a function that parses the argv string and returns the parsed value.
+
+	@example
+	```
+	type: String
+	```
+
+	@example Wrap in an array to accept multiple values.
+	```
+	type: [Boolean]
+	```
+
+	@example Custom function type that uses moment.js to parse string as date.
+	```
+	type: function CustomDate(value: string) {
+		return moment(value).toDate();
+	}
+	```
+	*/
 	type: TF;
+
+	/**
+	A single-character alias for the flag.
+
+	@example
+	```
+	alias: 's'
+	```
+	*/
 	alias?: string;
 };
 
 type FlagSchemaDefault<TF, DefaultType = any> = FlagSchemaBase<TF> & {
+	/**
+	Default value of the flag. Also accepts a function that returns the default value.
+	[Default: undefined]
+
+	@example
+	```
+	default: 'hello'
+	```
+
+	@example
+	```
+	default: () => [1, 2, 3]
+	```
+	*/
 	default: DefaultType | (() => DefaultType);
 };
 
