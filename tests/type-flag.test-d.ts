f33ea7a90c862b43bf818f6b5898a99d5684d101
Hiroki Osame <hiroki.osame@gmail.com>
2022-01-24 08:35:46 -0500

    test: added dts tests

---
+++ tests/type-flag.test-d.ts
@@ -0,0 +1,44 @@
+import { expectType } from 'tsd';
+import typeFlag from '../dist/index.js';
+
+const parsed = typeFlag({
+	booleanFlag: Boolean,
+	booleanFlagDefault: {
+		type: Boolean,
+		default: false,
+	},
+	stringFlag: String,
+	stringFlagDefault: {
+		type: String,
+		default: 'hello',
+	},
+	numberFlag: Number,
+	numberFlagDefault: {
+		type: Number,
+		default: 1,
+	},
+	extraOptions: {
+		type: Boolean,
+		alias: 'e',
+		default: false,
+		description: 'Some description',
+	},
+});
+
+type ExpectedType = {
+	flags: {
+		booleanFlag: boolean | undefined;
+		booleanFlagDefault: boolean;
+		stringFlag: string | undefined;
+		stringFlagDefault: string;
+		numberFlag: number | undefined;
+		numberFlagDefault: number;
+		extraOptions: boolean;
+	};
+	unknownFlags: {
+		[flag: string]:(string | boolean)[];
+	};
+	_: string[];
+};
+
+expectType<ExpectedType>(parsed);
